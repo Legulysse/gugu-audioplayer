c2573155e9591126565e2b8f5042b0467da67c24
Legulysse <legulysse@hotmail.com>
2022-01-06 15:03:43 +0100

    Create new project without Qt, archive previous build

---
+++ Tools/Build-premake.lua
@@ -0,0 +1,49 @@
+
+-- Setup the include path of the engine build scripts directory
+package.path = package.path .. ";../GuguEngine/Tools/Build/?.lua"
+
+require "PremakeEngine"
+require "PremakeProjects"
+
+
+-- Solution Configuration
+local pathDev 		= EnsureSlash("../Dev")
+local pathDevEngine = EnsureSlash("../GuguEngine/Dev")
+local pathSolution 	= EnsureSlash(pathDev.."Build/".._ACTION)
+local pathVersion 	= EnsureSlash("../Version")
+
+BuildCfg = {
+    -- Solution
+    DirSolution     = EnsureSlash(pathSolution),
+    SubDirBinaries  = EnsureSlash("Build_".._ACTION),
+    
+    -- Engine
+    DirSourcesEngine    = EnsureSlash(pathDevEngine.."SourcesEngine"),
+    DirSourcesSfml      = EnsureSlash(pathDevEngine.."SourcesSFML"),
+    DirSourcesPugiXml   = EnsureSlash(pathDevEngine.."SourcesPugiXml"),
+    DirSourcesImGui     = EnsureSlash(pathDevEngine.."SourcesImGui"),
+    DirLibEngine        = EnsureSlash(pathSolution.."Build"),
+}
+
+
+-- Build Solution
+solution "AudioPlayer"
+
+    location (BuildCfg.DirSolution)
+    package.guid = "BEBB4888-E0C7-482A-9304-DD406237C589"
+    configurations { "Debug", "Release" }
+    platforms { "x86", "x64" }
+    cppdialect "c++14"
+    
+    -- GUIDs can be generated from here : https://www.guidgenerator.com/online-guid-generator.aspx
+
+    group "Application"
+    ProjectDefault(BuildCfg, "GuguAudioPlayer", pathDev.."Sources", pathVersion, "6A983AD1-4D36-4949-B95D-B2FA4E7BC81D")
+    
+    group "Engine"
+    ProjectLibGuguEngine(BuildCfg)
+    
+    group "Externals"
+    ProjectLibSFML(BuildCfg)
+    ProjectLibPugiXml(BuildCfg)
+    ProjectLibImGui(BuildCfg)
